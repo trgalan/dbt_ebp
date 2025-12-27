6f0dc759e56bbbe16692976f71fa9ee1e956bb6a
Thomas Galan <trgalan@sbcglobal.net>
2025-12-27 01:13:59 +0000

    2025-12-26F

--- models/brz_engines_01.sql
+++ models/brz_engines_01.sql
@@ -1,15 +1,14 @@
 {{ config(
-    materialized = 'streaming_table',   -- DLT-backed incremental table
-    schema = 'bronze',
-    alias = 'brz_engines',
-    tblproperties = {
-      'quality': 'bronze',
-      'pipelines.autoOptimize.managed': 'true'
-    }
+  materialized='streaming_table',
+  schema='bronze',
+  alias='brz_engines',
+  tblproperties={
+    'quality': 'bronze',
+    'pipelines.autoOptimize.managed': 'true'
+  }
 ) }}
 
 with src as (
-
   select
     cast(engine_id as string)            as engine_id,
     cast(engine_model_id as string)      as engine_model_id,
@@ -18,15 +17,10 @@ with src as (
     cast(operator_customer as string)    as operator_customer,
     to_date(in_service_date, 'M/d/yyyy') as in_service_date,
     cast(configuration_status as string) as configuration_status,
-
-    -- lineage/audit (adjust if engine_01 already has these columns)
     current_timestamp()                  as ingest_ts,
     'dbt_ebp.raw_data.engine_01'         as source_table
-
   from stream {{ source('raw_data', 'engine_01') }}
-
 )
-
 select *
 from src
 where engine_id is not null;
