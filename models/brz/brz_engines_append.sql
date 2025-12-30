a182be47bcfa5fa941b08692a6a46f5e36edb47f
Thomas Galan <trgalan@sbcglobal.net>
2025-12-30 23:19:51 +0000

    2025-12-30C

--- models/brz/brz_engines_append.sql
+++ models/brz/brz_engines_append.sql
@@ -7,30 +7,28 @@
 with src as (
   select *
   from {{ source('raw_data', 'engine_01') }}
+),
+staged as (
+  select
+    cast(engine_id as string)            as engine_id,
+    cast(engine_model_id as string)      as engine_model_id,
+    cast(serial_number as string)        as serial_number,
+    cast(thrust_class as string)         as thrust_class,
+    cast(operator_customer as string)    as operator_customer,
+    to_date(in_service_date, 'M/d/yyyy') as in_service_date,
+    cast(configuration_status as string) as configuration_status,
+
+    cast(current_timestamp()  as timestamp)        as arrival_ts,
+    current_timestamp()                  as ingest_ts,
+    'raw_data.engine_01'                 as source_relation
+  from src
 )
 
-select
-  cast(engine_id as string)            as engine_id,
-  cast(engine_model_id as string)      as engine_model_id,
-  cast(serial_number as string)        as serial_number,
-  cast(thrust_class as string)         as thrust_class,
-  cast(operator_customer as string)    as operator_customer,
-  to_date(in_service_date, 'M/d/yyyy') as in_service_date,
-  cast(configuration_status as string) as configuration_status,
-
-  cast(arrival_ts as timestamp)        as arrival_ts,   -- persisted ingest timestamp
-  current_timestamp()                  as ingest_ts,
-  'raw_data.engine_01'                 as source_relation
-from src
+select *
+from staged
 
 {% if is_incremental() %}
-where cast(arrival_ts as timestamp) >
-      (select max(arrival_ts) from {{ this }})
+where staged.arrival_ts >
+      (select coalesce(max(t.arrival_ts), cast('1900-01-01 00:00:00' as timestamp)) from {{ this }} t)
 {% endif %}
-;
-
--- {% if is_incremental() %}
--- where to_timestamp(current_date()) >
---       (select max(arrival_ts) from {{ this }})
--- {% endif %}
 ;
