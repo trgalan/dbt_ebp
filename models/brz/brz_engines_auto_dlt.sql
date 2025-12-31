083f3530bba5beb020d0b620c79e8995597a007e
Thomas Galan <trgalan@sbcglobal.net>
2025-12-31 17:57:22 +0000

    2025-12-31D

--- models/brz/brz_engines_auto_dlt.sql
+++ models/brz/brz_engines_auto_dlt.sql
@@ -1,33 +1,43 @@
 {{ config(
-    materialized = "streaming_table",
-    schema = "dbt_ebp",
-    alias  = "brz_engines_auto_dlt",
-    tblproperties = {
-      "quality": "bronze",
-      "pipelines.autoOptimize.managed": "true"
+    materialized='streaming_table',
+    schema='dbt_brz',
+    alias='brz_engines_auto_dlt',
+    tblproperties={
+      'quality': 'bronze',
+      'pipelines.autoOptimize.managed': 'true'
     }
 ) }}
 
 SELECT
-  CAST(engine_id            AS STRING)            AS engine_id,
-  CAST(engine_model_id      AS STRING)            AS engine_model_id,
-  CAST(serial_number        AS STRING)            AS serial_number,
-  CAST(thrust_class         AS STRING)            AS thrust_class,
-  CAST(operator_customer    AS STRING)            AS operator_customer,
-  TO_DATE(in_service_date, 'M/d/yyyy')            AS in_service_date,
-  CAST(configuration_status AS STRING)            AS configuration_status,
+  CAST(engine_id            AS STRING) AS engine_id,
+  CAST(engine_model_id      AS STRING) AS engine_model_id,
+  CAST(serial_number        AS STRING) AS serial_number,
+  CAST(thrust_class         AS STRING) AS thrust_class,
+  CAST(operator_customer    AS STRING) AS operator_customer,
 
-  _metadata.file_modification_time                AS ingest_ts,
-  _metadata.file_path                             AS source_file,
-  _rescued                                        AS _rescue
+  try_cast(
+    CASE
+      WHEN in_service_date RLIKE '^[0-9]{4}-[0-9]{2}-[0-9]{2}$' THEN in_service_date
+      WHEN in_service_date RLIKE '^[0-9]{1,2}/[0-9]{1,2}/[0-9]{4}$' THEN
+        concat(
+          regexp_extract(in_service_date, '([0-9]{4})$', 1), '-',
+          lpad(regexp_extract(in_service_date, '^([0-9]{1,2})/', 1), 2, '0'), '-',
+          lpad(regexp_extract(in_service_date, '/([0-9]{1,2})/', 1), 2, '0')
+        )
+      ELSE NULL
+    END
+    AS DATE
+  ) AS in_service_date,
+
+  CAST(configuration_status AS STRING) AS configuration_status,
+  _metadata.file_modification_time AS ingest_ts,
+  _metadata.file_path              AS source_file,
+  _rescued                         AS _rescue
 
 FROM STREAM read_files(
-  -- Prefer the *directory* for streaming ingestion (more reliable than a single file path)
   "abfss://root@0815sa.dfs.core.windows.net/p30s_ebp/p30s_ebp_dev/inbound/",
   format => "csv",
   header => "true",
-
-  -- Explicit schema => no inference => no CF_EMPTY_DIR_FOR_SCHEMA_INFERENCE
   schema => "engine_id STRING,
              engine_model_id STRING,
              serial_number STRING,
@@ -35,8 +45,6 @@ FROM STREAM read_files(
              operator_customer STRING,
              in_service_date STRING,
              configuration_status STRING",
-
-  schemaEvolutionMode => "addNewColumns",
-  rescuedDataColumn   => "_rescued",
-  mode                => "PERMISSIVE"
+  rescuedDataColumn => "_rescued",
+  mode => "PERMISSIVE"
 );
