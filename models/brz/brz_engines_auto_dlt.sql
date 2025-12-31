a3fca65c4d696510ce2d022a3231f6414966de59
Thomas Galan <trgalan@sbcglobal.net>
2025-12-31 15:03:21 +0000

    2025-12-31-B

--- models/brz/brz_engines_auto_dlt.sql
+++ models/brz/brz_engines_auto_dlt.sql
@@ -9,27 +9,34 @@
 ) }}
 
 SELECT
-    CAST(engine_id            AS STRING)            AS engine_id,
-    CAST(engine_model_id      AS STRING)            AS engine_model_id,
-    CAST(serial_number        AS STRING)            AS serial_number,
-    CAST(thrust_class         AS STRING)            AS thrust_class,
-    CAST(operator_customer    AS STRING)            AS operator_customer,
-    TO_DATE(in_service_date, 'M/d/yyyy')            AS in_service_date,
-    CAST(configuration_status AS STRING)            AS configuration_status,
+  CAST(engine_id            AS STRING)            AS engine_id,
+  CAST(engine_model_id      AS STRING)            AS engine_model_id,
+  CAST(serial_number        AS STRING)            AS serial_number,
+  CAST(thrust_class         AS STRING)            AS thrust_class,
+  CAST(operator_customer    AS STRING)            AS operator_customer,
+  TO_DATE(in_service_date, 'M/d/yyyy')            AS in_service_date,
+  CAST(configuration_status AS STRING)            AS configuration_status,
 
-    _metadata.file_modification_time                AS ingest_ts,
-    _metadata.file_path                             AS source_file,
+  _metadata.file_modification_time                AS ingest_ts,
+  _metadata.file_path                             AS source_file,
+  _rescued                                        AS _rescue
 
-    _rescued                                        AS _rescue
+FROM STREAM read_files(
+  -- Prefer the *directory* for streaming ingestion (more reliable than a single file path)
+  "abfss://root@0815sa.dfs.core.windows.net/p30s_ebp/p30s_ebp_dev/inbound/",
+  format => "csv",
+  header => "true",
 
-FROM STREAM cloud_files(
-  "abfss://root@0815sa.dfs.core.windows.net/p30s_ebp/p30s_ebp_dev/inbound/engine_01.csv",
-  "csv",
-  map(
-    "header", "true",
-    "cloudFiles.inferColumnTypes", "true",
-    "cloudFiles.schemaEvolutionMode", "addNewColumns",
-    "cloudFiles.schemaLocation", "abfss://root@0815sa.dfs.core.windows.net/p30s_ebp/p30s_ebp_dev/_schemas/brz_engines_dlt/",
-    "cloudFiles.rescuedDataColumn", "_rescued"
-  )
+  -- Explicit schema => no inference => no CF_EMPTY_DIR_FOR_SCHEMA_INFERENCE
+  schema => "engine_id STRING,
+             engine_model_id STRING,
+             serial_number STRING,
+             thrust_class STRING,
+             operator_customer STRING,
+             in_service_date STRING,
+             configuration_status STRING",
+
+  schemaEvolutionMode => "addNewColumns",
+  rescuedDataColumn   => "_rescued",
+  mode                => "PERMISSIVE"
 );
